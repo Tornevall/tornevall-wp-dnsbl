06ac02124744e3f00812785af9ed90545dcca5eb
Thomas Tornevall <thorne@tornevall.net>
2026-08-17 22:45:33 +0200

    Use generic test data naming

--- tornevall-wp-dnsbl.php
+++ tornevall-wp-dnsbl.php
@@ -27,7 +27,7 @@ foreach ([
     'includes/class-dnsbl-plugin.php',
     'includes/class-dnsbl-migrations.php',
     'includes/class-dnsbl-admin.php',
-    'includes/class-dnsbl-fraud-mock.php',
+    'includes/class-dnsbl-test-profiles.php',
     'includes/class-dnsbl-api-client.php',
     'includes/class-dnsbl-write-queue.php',
     'includes/dnsbl-utils.php',
@@ -41,7 +41,7 @@ if (is_admin()) {
     require_once TORNEVALL_DNSBL_PLUGIN_DIR . 'includes/dnsbl-admin.php';
     add_action('admin_menu', 'tornevall_wp_dnsbl_admin');
     add_action('admin_init', 'register_dnsbl_settings');
-    \Tornevall\Networks\DNSBL\FraudMock::registerHooks();
+    \Tornevall\Networks\DNSBL\TestProfiles::registerHooks();
 }
 
 register_activation_hook(TORNEVALL_DNSBL_PLUGIN_FILE, 'tornevall_wp_dnsbl_activate_db');
