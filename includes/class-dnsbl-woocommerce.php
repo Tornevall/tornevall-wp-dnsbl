1c21952c39baf994b1267bbf3c01cce37db4699e
Thomas Tornevall <thorne@tornevall.net>
2026-08-21 15:43:15 +0200

    Port WooCommerce checkout protection class to 3.2

---
+++ includes/class-dnsbl-woocommerce.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tornevall\Networks\DNSBL;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+foreach ([
+    'woo-commerce-core-trait.php',
+    'woo-commerce-settings-trait.php',
+    'woo-commerce-admin-trait.php',
+    'woo-commerce-checkout-trait.php',
+    'woo-commerce-notifications-trait.php',
+] as $tornevallDnsblWooCommerceTrait) {
+    require_once TORNEVALL_DNSBL_PLUGIN_DIR . 'includes/traits/' . $tornevallDnsblWooCommerceTrait;
+}
+
+class WooCommerce
+{
+    use WooCommerceCoreTrait;
+    use WooCommerceSettingsTrait;
+    use WooCommerceAdminTrait;
+    use WooCommerceCheckoutTrait;
+    use WooCommerceNotificationsTrait;
+
+    private const SETTINGS_PAGE = 'tornevallDnsblWooCommerce';
+    private const SETTINGS_GROUP = 'dnsblWooCommerceOptions-group';
+    private const CRON_HOOK = 'tornevall_dnsbl_wc_bulk_notification';
+    private const TABLE_NAME = 'tornevall_dnsbl_wc_blocked_log';
+}
