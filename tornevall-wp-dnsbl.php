c4ee5646eed2dc8030c7d9d9811fc5adf62c9cf6
Thomas Tornevall <thorne@tornevall.net>
2026-08-21 14:31:23 +0200

    Register direct Tools pairing in DNSBL plugin

--- tornevall-wp-dnsbl.php
+++ tornevall-wp-dnsbl.php
@@ -33,6 +33,7 @@ foreach ([
     'includes/class-dnsbl-write-queue.php',
     'includes/class-dnsbl-integration.php',
     'includes/class-dnsbl-managed-tools-token.php',
+    'includes/class-dnsbl-tools-pairing.php',
     'includes/class-dnsbl-telemetry.php',
     'includes/class-dnsbl-woocommerce.php',
     'includes/dnsbl-utils.php',
@@ -55,5 +56,6 @@ register_uninstall_hook(TORNEVALL_DNSBL_PLUGIN_FILE, 'tornevall_wp_dnsbl_uninsta
 tornevall_dnsbl_register_hooks();
 \Tornevall\Networks\DNSBL\Integration::registerHooks();
 \Tornevall\Networks\DNSBL\ManagedToolsToken::registerHooks();
+\Tornevall\Networks\DNSBL\ToolsPairing::registerHooks();
 \Tornevall\Networks\DNSBL\Telemetry::registerHooks();
 \Tornevall\Networks\DNSBL\WooCommerce::registerHooks();
